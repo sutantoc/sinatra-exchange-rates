cf100f6180c28d5e07d2cfed0f5f969f872362cd
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 05:10:35 +0000

    cur erb

--- app.rb
+++ app.rb
@@ -12,3 +12,15 @@ get("/") do
   @currency_array = @currencies_hash.keys
   erb(:home)
 end
+
+get ("/:cur") do
+  @cur = params.fetch("cur")
+  @api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  @raw = HTTP.get(@api_url)
+  @parsed = JSON.parse(@raw)
+  @currencies_hash = @parsed.fetch("currencies")
+  @currency_array = @currencies_hash.keys
+
+
+  erb(:cur)
+end
