10947f3e683e9b2eccca1d69d155983036de9385
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 05:18:49 +0000

    conversion erb

--- app.rb
+++ app.rb
@@ -20,7 +20,15 @@ get ("/:cur") do
   @parsed = JSON.parse(@raw)
   @currencies_hash = @parsed.fetch("currencies")
   @currency_array = @currencies_hash.keys
-
-
   erb(:cur)
+end
+
+get ("/:cur/:second_cur") do
+  @cur = params.fetch("cur")
+  @second_cur = params.fetch("second_cur")
+  @api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@cur}&to=#{@second_cur}&amount=1"
+  @raw = HTTP.get(@api_url)
+  @parsed = JSON.parse(@raw)
+  @result = @parsed.fetch("result")
+  erb(:conversion)
 end
